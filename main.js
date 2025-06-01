562130b469b2f691dce49c1f1e9915f8d9919e25
Lucas <lucasalassia1@outlook.com>
2025-06-01 17:40:14 -0300

    Hasta tarjetas

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const contenedorPaises = document.getElementById("listado");
 const contenedorBanderas = document.getElementById("cont-galeria");
 
 paisesMasPoblados(contenedorPaises);
-obtenerBanderas(contenedorBanderas);
 
 async function paisesMasPoblados(cont) {
   try {
@@ -30,14 +29,14 @@ async function paisesMasPoblados(cont) {
     const data = await response.json(); //En JavaScript, los datos JSON se manejan directamente como objetos. Data es el objeto completo.
 
     // Ordenar por población (de mayor a menor)
-    const sortedCountries = data.sort((a, b) => b.population - a.population);
+    const ordenPoblacion = data.sort((a, b) => b.population - a.population);
 
     // Tomar los 15 primeros
-    const ArrayPaises = sortedCountries.slice(0, 20);
+    const ArrayPaises = ordenPoblacion.slice(0, 20);
 
     ArrayPaises.forEach((pais) => {
       const div = document.createElement("div");
-      div.classList.add("card", "mb-3");
+      div.classList.add("col-6", "col-sm-6", "col-md-4", "col-lg-3");
 
       //Languaje lo convierto con Object. ya que el lenguaje tiene diferentes claves
       const lenguajeNativo = Object.values(pais.languages); //Object.values es una funcion que me genera un array de valores
@@ -45,12 +44,24 @@ async function paisesMasPoblados(cont) {
 
       //En esta Api para cada idioma y nombre hay dos formas: official y common
       div.innerHTML = `
-        <div class="card-body">
-            <h3 class="card-title">${pais.translations.spa.common}</h3> 
-            <p class="text-muted"> Capital: ${pais.capital}</p>
-            <p class="text-muted"> Continente: ${pais.continents} </p>
-            <p class="text-muted"> Lenguaje nativo: ${lengNativo}</p>
-            <p class="text-muted"> Poblacion: ${pais.population.toLocaleString()}</p>
+        <div class="card h-90"> 
+            <img src=${
+              pais.flags.png
+            } class="card-img-top img-fluid object-fit-cover border border-black""
+              alt="${pais.name.common}">
+            <div class="card-body">
+              <h5 class="card-title text-center">${
+                pais.translations.spa.common
+              }</h5> 
+              <p class="card-text"> <strong>Capital:</strong> ${
+                pais.capital
+              }</p>
+              <p class="card-text"> <strong>Continente:</strong> ${
+                pais.continents
+              } </p>
+              <p class="card-text"> <strong>Lenguaje nativo:</strong> ${lengNativo}</p>
+              <p class="card-text"> <strong>Poblacion:</strong> ${pais.population.toLocaleString()}</p>
+            </div>
         </div>
       `;
       cont.appendChild(div);
@@ -60,7 +71,7 @@ async function paisesMasPoblados(cont) {
   }
 }
 
-async function obtenerBanderas(cont) {
+/*async function obtenerBanderas(cont) {
   try {
     // Hacemos la solicitud HTTP (GET)
 
@@ -96,4 +107,4 @@ async function obtenerBanderas(cont) {
   } catch (error) {
     console.error("Error:", error);
   }
-}
+}*/
