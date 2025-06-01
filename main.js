09304954863d4530cad4b14a18694b27d8ffd9b0
Lucas <lucasalassia1@outlook.com>
2025-06-01 12:43:58 -0300

    Hasta comparacion de poblacion

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ boton.addEventListener("Onclick", (e) => {
 const contenedorPaises = document.getElementById("listado");
 const contenedorBanderas = document.getElementById("cont-galeria");
 
-obtenerPaises(contenedorPaises);
+paisesMasPoblados(contenedorPaises);
 obtenerBanderas(contenedorBanderas);
 
-async function obtenerPaises(cont) {
+async function paisesMasPoblados(cont) {
   try {
     // Hacemos la solicitud HTTP (GET)
 
@@ -28,11 +28,17 @@ async function obtenerPaises(cont) {
 
     // Convertimos la respuesta en JSON
     const data = await response.json(); //En JavaScript, los datos JSON se manejan directamente como objetos. Data es el objeto completo.
-    const ArrayPaises = data.slice(3, 15); //Elijo la cantidad de elementos a mostrar
+
+    // Ordenar por población (de mayor a menor)
+    const sortedCountries = data.sort((a, b) => b.population - a.population);
+
+    // Tomar los 15 primeros
+    const ArrayPaises = sortedCountries.slice(0, 20);
 
     ArrayPaises.forEach((pais) => {
       const div = document.createElement("div");
       div.classList.add("card", "mb-3");
+
       //Languaje lo convierto con Object. ya que el lenguaje tiene diferentes claves
       const lenguajeNativo = Object.values(pais.languages); //Object.values es una funcion que me genera un array de valores
       const lengNativo = lenguajeNativo[0];
@@ -40,10 +46,11 @@ async function obtenerPaises(cont) {
       //En esta Api para cada idioma y nombre hay dos formas: official y common
       div.innerHTML = `
         <div class="card-body">
-            <h3 class="card-title">${pais.name.common}</h3> 
+            <h3 class="card-title">${pais.translations.spa.common}</h3> 
             <p class="text-muted"> Capital: ${pais.capital}</p>
             <p class="text-muted"> Continente: ${pais.continents} </p>
             <p class="text-muted"> Lenguaje nativo: ${lengNativo}</p>
+            <p class="text-muted"> Poblacion: ${pais.population.toLocaleString()}</p>
         </div>
       `;
       cont.appendChild(div);
